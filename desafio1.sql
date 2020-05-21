becfdab5278ac188d60a0745a30f0e7f2ddbcaa6
guicgs <etohlif@protonmail.com>
2020-05-20 22:34:29 -0300

    desafio 9 concluído

--- desafio1.sql
+++ desafio1.sql
@@ -13,6 +13,8 @@ CREATE TABLE IF NOT EXISTS travel_packages (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     price DECIMAL(8 , 2) NOT NULL,
+    departure_date DATE NOT NULL,
+    arrive_date DATE NOT NULL,
     purchase_count INT DEFAULT 0
 )ENGINE=InnoDB;
 
@@ -25,8 +27,6 @@ CREATE TABLE IF NOT EXISTS locations (
 CREATE TABLE IF NOT EXISTS travel_packages_locations (
     travel_package_id INT NOT NULL,
     location_id INT NOT NULL,
-    departure_date DATE NOT NULL,
-    arrive_date DATE NOT NULL,
     PRIMARY KEY(travel_package_id, location_id),
     CONSTRAINT fk_travel_packages_locations_travel_packages FOREIGN KEY (travel_package_id)
         REFERENCES travel_packages (id)
@@ -56,13 +56,13 @@ VALUES
     ('Carol Domingues', 37, 'Contadora'),
     ('Sabrina Ferreira', 45, 'Engenheira de Alimentos');
 
-INSERT INTO travel_packages(name, price)
+INSERT INTO travel_packages(name, price, departure_date, arrive_date)
 VALUES 
-    ('Promoção 3 destinos', 12500.99),
-    ('Vem Pro Verão', 9650.99),
-    ('Cultura Francesa', 3900.99),
-    ('Vem Pro Verão', 7725.99),
-    ('Promoção 3 destinos', 13500.99);
+    ('Promoção 3 destinos', 12500.99, '2022-09-25', '2022-10-12'),
+    ('Vem Pro Verão', 9650.99, '2022-10-16', '2022-10-26'),
+    ('Cultura Francesa', 3900.99, '2022-07-12', '2022-07-20'),
+    ('Vem Pro Verão', 7725.99, '2022-02-19', '2022-03-26'),
+    ('Promoção 3 destinos', 13500.99, '2022-09-13', '2022-10-18');
     
 INSERT INTO locations(city, country)
 VALUES
@@ -73,19 +73,19 @@ VALUES
     ('Nova Iorque', 'Estados Unidos');
 
 INSERT INTO 
-    travel_packages_locations(travel_package_id, location_id, departure_date, arrive_date)
+    travel_packages_locations(travel_package_id, location_id)
 VALUES
-    (1, 1, '2022-09-25', '2022-10-12'),
-    (1, 2, '2022-09-25', '2022-10-12'),
-    (1, 3, '2022-09-25', '2022-10-12'),
-    (2, 4, '2022-10-16', '2022-10-26'),
-    (2, 5, '2022-10-16', '2022-10-26'),
-    (3, 4, '2022-07-12', '2022-07-20'),
-    (4, 1, '2022-02-19', '2022-03-26'),
-    (4, 2, '2022-02-19', '2022-03-26'),
-    (5, 3, '2022-09-13', '2022-10-18'),
-    (5, 5, '2022-09-13', '2022-10-18'),
-    (5, 4, '2022-09-13', '2022-10-18');
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 4),
+    (2, 5),
+    (3, 4),
+    (4, 1),
+    (4, 2),
+    (5, 3),
+    (5, 5),
+    (5, 4);
     
 
 
