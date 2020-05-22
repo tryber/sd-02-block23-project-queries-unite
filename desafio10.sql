7c73eaff5019509a963e1b95c29e4aac066c09f9
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-05-22 13:39:43 -0300

    CC

--- desafio10.sql
+++ desafio10.sql
@@ -8,13 +8,13 @@ CREATE FUNCTION average_amount_spent_given_age_range(min_age int, max_age int)
 RETURNS DOUBLE READS SQL DATA
 BEGIN
 return
-	(
-	select avg(tp.total_price)
-	from travel_packages as tp
-	inner join purchases as pu on pu.travel_id = tp.id
-	inner join users as us on pu.user_id = us.id
-	where us.current_age between min_age and max_age
-			);
+(
+select avg(tp.total_price)
+from travel_packages as tp
+inner join purchases as pu on pu.travel_id = tp.id
+inner join users as us on pu.user_id = us.id
+where us.current_age between min_age and max_age
+);
 END $$;
 
 DELIMITER ;
