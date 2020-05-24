6e3ee5e290ecd68fbfd6667062b8924fd65f2e57
richardwellerson <richardwellerson@gmail.com>
2020-05-24 14:00:49 -0300

    Desafio 8 feito

---
+++ desafios/desafio8.sql
@@ -0,0 +1,7 @@
+CREATE VIEW revenue_report AS
+SELECT
+MIN(price * purchase_count) AS lowest_revenue,
+MAX(price * purchase_count) AS biggest_revenue,
+AVG(price * purchase_count) AS average_revenue,
+SUM(price * purchase_count) AS total_revenue
+FROM travel_packages;
