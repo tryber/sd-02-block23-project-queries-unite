ea25c7a0c54b5d6f3f2709329a97826faf11855c
richardwellerson <richardwellerson@gmail.com>
2020-05-24 13:52:30 -0300

    Desafio 7 feito

---
+++ desafios/desafio7.sql
@@ -0,0 +1,12 @@
+CREATE VIEW top_3_locations AS
+SELECT
+tloc.location_id as id,
+loc.city,
+loc.country,
+COUNT(pur.travel_packages_id) as travel_amount
+FROM travel_packages_locations as tloc
+INNER JOIN locations as loc on tloc.location_id = loc.id
+INNER JOIN purchases as pur on pur.travel_packages_id = tloc.travel_packages_id
+GROUP BY loc.id
+ORDER BY travel_amount DESC
+LIMIT 3;
