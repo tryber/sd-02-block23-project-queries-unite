76610749a0bb8f26300fdec4441ff78af0e60fb5
mateustalles <mateustalles@gmail.com>
2020-05-25 19:51:59 -0300

    Updated 'package_id' name to 'travel_package_id'

--- desafio1.sql
+++ desafio1.sql
@@ -10,8 +10,8 @@ CREATE TABLE users(
 )engine=InnoDB;
 
 CREATE TABLE travel_packages(
-	package_id INT PRIMARY KEY auto_increment,
-    package_name VARCHAR(50) UNIQUE NOT NULL,
+	travel_package_id INT PRIMARY KEY auto_increment,
+    travel_package_name VARCHAR(50) UNIQUE NOT NULL,
     purchase_count TINYINT SIGNED NOT NULL DEFAULT 0
 )engine=InnoDB;
 
@@ -23,11 +23,11 @@ CREATE TABLE locations(
 
 CREATE TABLE travel_packages_locations(
 	travel_packages_locations INT PRIMARY KEY auto_increment,
-	package_id INT NOT NULL,
+	travel_package_id INT NOT NULL,
     location_id_1 INT NOT NULL,
     location_id_2 INT,
     location_id_3 INT,
-	FOREIGN KEY (package_id) REFERENCES travel_packages(package_id),
+	FOREIGN KEY (travel_package_id) REFERENCES travel_packages(travel_package_id),
     FOREIGN KEY (location_id_1) REFERENCES locations(location_id),
     FOREIGN KEY (location_id_2) REFERENCES locations(location_id),
     FOREIGN KEY (location_id_3) REFERENCES locations(location_id)
@@ -36,12 +36,12 @@ CREATE TABLE travel_packages_locations(
 CREATE TABLE purchases(
 	purchase_id INT PRIMARY KEY auto_increment,
     user_id INT NOT NULL,
-    package_id INT NOT NULL,
+    travel_package_id INT NOT NULL,
     departure_date DATE NOT NULL,
 	arrival_date DATE NOT NULL,
     price DECIMAL(7,2) NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES users(id),
-	FOREIGN KEY (package_id) REFERENCES travel_packages(package_id)
+	FOREIGN KEY (travel_package_id) REFERENCES travel_packages(travel_package_id)
 )engine=InnoDB;
 
 
@@ -52,7 +52,7 @@ INSERT INTO users (full_name, current_age, current_job)
               ('Carol Domingues', 37, 'Contadora'),
               ('Sabrina Ferreira', 45, 'Engenheira de Alimentos');
 
-INSERT INTO travel_packages (package_name, purchase_count)
+INSERT INTO travel_packages (travel_package_name, purchase_count)
        VALUES ('Promoção 3 destinos', 2),
               ('Vem Pro Verão', 2),
               ('Cultura Francesa', 2);
@@ -64,18 +64,18 @@ INSERT INTO locations (location_city, location_country)
               ('Paris', 'França'),
               ('Nova Iorque', 'Estados Unidos');
               
-INSERT INTO travel_packages_locations (package_id, location_id_1, location_id_2, location_id_3)
+INSERT INTO travel_packages_locations (travel_package_id, location_id_1, location_id_2, location_id_3)
 	VALUES (1, 1, 2, 3),
            (2, 4, 5, NULL),
            (3, 4, NULL, NULL),
            (2, 1, 2, NULL),
            (1, 3, 5, 4);
            
-INSERT INTO purchases (user_id, package_id, departure_date, arrival_date, price)
+INSERT INTO purchases (user_id, travel_package_id, departure_date, arrival_date, price)
 	VALUES (1, 1, '2022-09-25', '2022-10-12', 12500.99),
 		   (2, 2, '2022-10-16', '2022-10-26', 9650.99),
            (3, 3, '2022-07-12', '2022-07-20', 3900.00),
            (4, 2, '2022-02-19', '2022-03-26', 7725.99),
            (5, 3, '2022-07-12', '2022-07-20', 3900.99),
            (5, 1, '2022-09-13', '2022-10-18', 13500.99)
-	
+           
