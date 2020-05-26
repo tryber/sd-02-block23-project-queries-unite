f4e040cc957e502b7f0827b7b5685d8489ad5e62
mateustalles <mateustalles@gmail.com>
2020-05-26 19:53:55 -0300

    Desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,14 @@
+DROP TRIGGER IF EXISTS increment_travel_package_purchases;
+
+DELIMITER $$
+
+CREATE TRIGGER increment_travel_package_purchases
+	AFTER INSERT ON purchases 
+	FOR EACH ROW
+BEGIN
+	UPDATE travel_packages_locations
+    SET purchase_count = purchase_count + 1
+    WHERE travel_packages_locations_id = NEW.travel_packages_locations_id;
+END $$
+
+DELIMITER ;
